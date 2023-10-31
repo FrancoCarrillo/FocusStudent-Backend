ef1374074304b95ba6c3a77b258709f539557bf1
Franco Carrillo <enc_fcarrillo@niubiz.com.pe>
2023-10-30 23:57:24 -0500

    feat: add localhost port cors

--- src/main/java/com/codetech/focusstudentbackend/api/config/SecurityConfig.java
+++ src/main/java/com/codetech/focusstudentbackend/api/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200", "https://focus-student.netlify.app"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost:3000", "https://focus-student.netlify.app"));
         configuration.setAllowedOriginPatterns(List.of("http://localhost:4200", "https://inmobipay.netlify.app"));
         configuration.setAllowedMethods(List.of("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
         configuration.setAllowedHeaders(List.of("Access-Control-Allow-Origin", "X-Requested-With", "Origin", "Content-Type", "Accept", "Authorization"));
